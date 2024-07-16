439bcfab5fcf1c6f8b707ad1207ab09e7396f509
easyhz <jiton3@naver.com>
2024-07-16 13:52:40 +0900

    :tada: init project

---
+++ app/src/main/java/com/afs/turtd/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.afs.turtd
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.afs.turtd.base.BaseActivity
+import com.afs.turtd.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun initView() {
+
+    }
+}
