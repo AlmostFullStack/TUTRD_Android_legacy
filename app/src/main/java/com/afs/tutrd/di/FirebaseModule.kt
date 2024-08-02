941c31d484531e8a02b71e5672bedb2435b0eacd
easyhz <jiton3@naver.com>
2024-08-02 23:44:32 +0900

    :wrench: chore: firebase di 추가

---
+++ app/src/main/java/com/afs/tutrd/di/FirebaseModule.kt
@@ -0,0 +1,28 @@
+package com.afs.tutrd.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFireStore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFireBaseStorage(): FirebaseStorage {
+        return Firebase.storage
+    }
+}
